5751113605bec1241671c6688bd01eb34e9f2084
Samad-Ali-S <fa23bese0016@maju.edu.pk>
2024-12-24 01:04:14 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         <div>
             <h3 className="text-md font-semibold mb-4">Resources</h3>
-            <ul className="space-y-2">
+            <ul className="space-y-2"> 
                 {resourcesLinks.map((link) => (
                     <li key={link.href}>
                         <a 
